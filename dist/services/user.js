b0204529c5d4dfbbf7ee30d9230e66f41dec8a15
Higor Braga <higorbraga.s@gmail.com>
2024-11-20 18:54:13 -0300

    feat: add name user

--- dist/services/user.js
+++ dist/services/user.js
@@ -7,69 +7,83 @@ var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __propIsEnum = Object.prototype.propertyIsEnumerable;
-var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __defNormalProp = (obj, key, value) =>
+   key in obj
+      ? __defProp(obj, key, {
+           enumerable: true,
+           configurable: true,
+           writable: true,
+           value,
+        })
+      : (obj[key] = value);
 var __spreadValues = (a, b) => {
-  for (var prop in b || (b = {}))
-    if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
-  if (__getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
-    }
-  return a;
+   for (var prop in b || (b = {}))
+      if (__hasOwnProp.call(b, prop)) __defNormalProp(a, prop, b[prop]);
+   if (__getOwnPropSymbols)
+      for (var prop of __getOwnPropSymbols(b)) {
+         if (__propIsEnum.call(b, prop)) __defNormalProp(a, prop, b[prop]);
+      }
+   return a;
 };
 var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
+   for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
 };
 var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
+   if ((from && typeof from === "object") || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+         if (!__hasOwnProp.call(to, key) && key !== except)
+            __defProp(to, key, {
+               get: () => from[key],
+               enumerable:
+                  !(desc = __getOwnPropDesc(from, key)) || desc.enumerable,
+            });
+   }
+   return to;
 };
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var __toCommonJS = (mod) =>
+   __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var __async = (__this, __arguments, generator) => {
-  return new Promise((resolve, reject) => {
-    var fulfilled = (value) => {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
-      }
-    };
-    var rejected = (value) => {
-      try {
-        step(generator.throw(value));
-      } catch (e) {
-        reject(e);
-      }
-    };
-    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
-    step((generator = generator.apply(__this, __arguments)).next());
-  });
+   return new Promise((resolve, reject) => {
+      var fulfilled = (value) => {
+         try {
+            step(generator.next(value));
+         } catch (e) {
+            reject(e);
+         }
+      };
+      var rejected = (value) => {
+         try {
+            step(generator.throw(value));
+         } catch (e) {
+            reject(e);
+         }
+      };
+      var step = (x) =>
+         x.done
+            ? resolve(x.value)
+            : Promise.resolve(x.value).then(fulfilled, rejected);
+      step((generator = generator.apply(__this, __arguments)).next());
+   });
 };
 
 // src/services/user.ts
 var user_exports = {};
 __export(user_exports, {
-  checkIfFollows: () => checkIfFollows,
-  createUser: () => createUser,
-  findPostsByUser: () => findPostsByUser,
-  findUserByEmail: () => findUserByEmail,
-  findUserBySlug: () => findUserBySlug,
-  followUser: () => followUser,
-  getFollowSuggestions: () => getFollowSuggestions,
-  getUserFollowersCount: () => getUserFollowersCount,
-  getUserFollowing: () => getUserFollowing,
-  getUserFollowingCount: () => getUserFollowingCount,
-  getUserPostsCount: () => getUserPostsCount,
-  unfollowUser: () => unfollowUser,
-  updateUserInfo: () => updateUserInfo
+   checkIfFollows: () => checkIfFollows,
+   createUser: () => createUser,
+   findPostsByUser: () => findPostsByUser,
+   findUserByEmail: () => findUserByEmail,
+   findUserBySlug: () => findUserBySlug,
+   followUser: () => followUser,
+   getFollowSuggestions: () => getFollowSuggestions,
+   getUserFollowersCount: () => getUserFollowersCount,
+   getUserFollowing: () => getUserFollowing,
+   getUserFollowingCount: () => getUserFollowingCount,
+   getUserPostsCount: () => getUserPostsCount,
+   unfollowUser: () => unfollowUser,
+   updateUserInfo: () => updateUserInfo,
 });
 module.exports = __toCommonJS(user_exports);
 
@@ -81,121 +95,135 @@ if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 // src/utils/url.ts
 var getPublicURL = (url) => {
-  return `${process.env.BASE_URL}/${url}`;
+   return `${process.env.BASE_URL}/${url}`;
 };
 
 // src/services/user.ts
-var findUserByEmail = (email) => __async(void 0, null, function* () {
-  const user = yield prisma.user.findFirst({
-    where: { email }
-  });
-  if (user) {
-    return __spreadProps(__spreadValues({}, user), {
-      avatar: getPublicURL(user.avatar),
-      cover: getPublicURL(user.cover)
-    });
-  }
-  return null;
-});
-var findUserBySlug = (slug) => __async(void 0, null, function* () {
-  const user = yield prisma.user.findFirst({
-    select: {
-      avatar: true,
-      cover: true,
-      slug: true,
-      bio: true,
-      link: true
-    },
-    where: { slug }
-  });
-  if (user) {
-    return __spreadProps(__spreadValues({}, user), {
-      avatar: getPublicURL(user.avatar),
-      cover: getPublicURL(user.cover)
-    });
-  }
-  return null;
-});
-var createUser = (data) => __async(void 0, null, function* () {
-  const newUser = yield prisma.user.create({ data });
-  return __spreadProps(__spreadValues({}, newUser), {
-    avatar: getPublicURL(newUser.avatar),
-    cover: getPublicURL(newUser.cover)
-  });
-});
-var getUserFollowingCount = (slug) => __async(void 0, null, function* () {
-  const count = yield prisma.follow.count({
-    where: { user1Slug: slug }
-  });
-  return count;
-});
-var getUserFollowersCount = (slug) => __async(void 0, null, function* () {
-  const count = yield prisma.follow.count({
-    where: { user2Slug: slug }
-  });
-  return count;
-});
-var getUserPostsCount = (slug) => __async(void 0, null, function* () {
-  const count = yield prisma.post.count({
-    where: { userSlug: slug }
-  });
-  return count;
-});
-var findPostsByUser = (slug, currentPage, perPage) => __async(void 0, null, function* () {
-  const posts = yield prisma.post.findMany({
-    include: {
-      likes: {
-        select: {
-          userSlug: true
-        }
+var findUserByEmail = (email) =>
+   __async(void 0, null, function* () {
+      const user = yield prisma.user.findFirst({
+         where: { email },
+      });
+      if (user) {
+         return __spreadProps(__spreadValues({}, user), {
+            avatar: getPublicURL(user.avatar),
+            cover: getPublicURL(user.cover),
+         });
       }
-    },
-    where: { userSlug: slug, commentOf: 0 },
-    orderBy: { createdAt: "desc" },
-    skip: currentPage * perPage,
-    take: perPage
-  });
-  return posts;
-});
-var checkIfFollows = (user1Slug, user2Slug) => __async(void 0, null, function* () {
-  const follows = yield prisma.follow.findFirst({
-    where: { user1Slug, user2Slug }
-  });
-  return follows ? true : false;
-});
-var followUser = (user1Slug, user2Slug) => __async(void 0, null, function* () {
-  yield prisma.follow.create({
-    data: { user1Slug, user2Slug }
-  });
-});
-var unfollowUser = (user1Slug, user2Slug) => __async(void 0, null, function* () {
-  yield prisma.follow.deleteMany({
-    where: { user1Slug, user2Slug }
-  });
-});
-var updateUserInfo = (slug, data) => __async(void 0, null, function* () {
-  yield prisma.user.update({
-    where: { slug },
-    data
-  });
-});
-var getUserFollowing = (slug) => __async(void 0, null, function* () {
-  const following = [];
-  const reqFollow = yield prisma.follow.findMany({
-    select: {
-      user2Slug: true
-    },
-    where: { user1Slug: slug }
-  });
-  for (let follow of reqFollow) {
-    following.push(follow.user2Slug);
-  }
-  return following;
-});
-var getFollowSuggestions = (slug) => __async(void 0, null, function* () {
-  const following = yield getUserFollowing(slug);
-  const followingPlusMe = [...following, slug];
-  const suggestions = yield prisma.$queryRaw`
+      return null;
+   });
+var findUserBySlug = (slug) =>
+   __async(void 0, null, function* () {
+      const user = yield prisma.user.findFirst({
+         select: {
+            avatar: true,
+            cover: true,
+            slug: true,
+            bio: true,
+            link: true,
+            name: true,
+         },
+         where: { slug },
+      });
+      if (user) {
+         return __spreadProps(__spreadValues({}, user), {
+            avatar: getPublicURL(user.avatar),
+            cover: getPublicURL(user.cover),
+         });
+      }
+      return null;
+   });
+var createUser = (data) =>
+   __async(void 0, null, function* () {
+      const newUser = yield prisma.user.create({ data });
+      return __spreadProps(__spreadValues({}, newUser), {
+         avatar: getPublicURL(newUser.avatar),
+         cover: getPublicURL(newUser.cover),
+      });
+   });
+var getUserFollowingCount = (slug) =>
+   __async(void 0, null, function* () {
+      const count = yield prisma.follow.count({
+         where: { user1Slug: slug },
+      });
+      return count;
+   });
+var getUserFollowersCount = (slug) =>
+   __async(void 0, null, function* () {
+      const count = yield prisma.follow.count({
+         where: { user2Slug: slug },
+      });
+      return count;
+   });
+var getUserPostsCount = (slug) =>
+   __async(void 0, null, function* () {
+      const count = yield prisma.post.count({
+         where: { userSlug: slug },
+      });
+      return count;
+   });
+var findPostsByUser = (slug, currentPage, perPage) =>
+   __async(void 0, null, function* () {
+      const posts = yield prisma.post.findMany({
+         include: {
+            likes: {
+               select: {
+                  userSlug: true,
+               },
+            },
+         },
+         where: { userSlug: slug, commentOf: 0 },
+         orderBy: { createdAt: "desc" },
+         skip: currentPage * perPage,
+         take: perPage,
+      });
+      return posts;
+   });
+var checkIfFollows = (user1Slug, user2Slug) =>
+   __async(void 0, null, function* () {
+      const follows = yield prisma.follow.findFirst({
+         where: { user1Slug, user2Slug },
+      });
+      return follows ? true : false;
+   });
+var followUser = (user1Slug, user2Slug) =>
+   __async(void 0, null, function* () {
+      yield prisma.follow.create({
+         data: { user1Slug, user2Slug },
+      });
+   });
+var unfollowUser = (user1Slug, user2Slug) =>
+   __async(void 0, null, function* () {
+      yield prisma.follow.deleteMany({
+         where: { user1Slug, user2Slug },
+      });
+   });
+var updateUserInfo = (slug, data) =>
+   __async(void 0, null, function* () {
+      yield prisma.user.update({
+         where: { slug },
+         data,
+      });
+   });
+var getUserFollowing = (slug) =>
+   __async(void 0, null, function* () {
+      const following = [];
+      const reqFollow = yield prisma.follow.findMany({
+         select: {
+            user2Slug: true,
+         },
+         where: { user1Slug: slug },
+      });
+      for (let follow of reqFollow) {
+         following.push(follow.user2Slug);
+      }
+      return following;
+   });
+var getFollowSuggestions = (slug) =>
+   __async(void 0, null, function* () {
+      const following = yield getUserFollowing(slug);
+      const followingPlusMe = [...following, slug];
+      const suggestions = yield prisma.$queryRaw`
         SELECT
             name, avatar, slug
         FROM "User"
@@ -204,23 +232,26 @@ var getFollowSuggestions = (slug) => __async(void 0, null, function* () {
         ORDER BY RANDOM()
         LIMIT 2;
     `;
-  for (let slugIndex in suggestions) {
-    suggestions[slugIndex].avatar = getPublicURL(suggestions[slugIndex].avatar);
-  }
-});
+      for (let slugIndex in suggestions) {
+         suggestions[slugIndex].avatar = getPublicURL(
+            suggestions[slugIndex].avatar
+         );
+      }
+   });
 // Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
-  checkIfFollows,
-  createUser,
-  findPostsByUser,
-  findUserByEmail,
-  findUserBySlug,
-  followUser,
-  getFollowSuggestions,
-  getUserFollowersCount,
-  getUserFollowing,
-  getUserFollowingCount,
-  getUserPostsCount,
-  unfollowUser,
-  updateUserInfo
-});
+0 &&
+   (module.exports = {
+      checkIfFollows,
+      createUser,
+      findPostsByUser,
+      findUserByEmail,
+      findUserBySlug,
+      followUser,
+      getFollowSuggestions,
+      getUserFollowersCount,
+      getUserFollowing,
+      getUserFollowingCount,
+      getUserPostsCount,
+      unfollowUser,
+      updateUserInfo,
+   });
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,174 +1,183 @@
 import { Prisma } from "@prisma/client";
-import { prisma } from "../utils/prisma"
+import { prisma } from "../utils/prisma";
 import { getPublicURL } from "../utils/url";
 
-export const findUserByEmail = async (email:string) => {
-    const user = await prisma.user.findFirst({
-        where: { email }
-    });
+export const findUserByEmail = async (email: string) => {
+   const user = await prisma.user.findFirst({
+      where: { email },
+   });
 
-    if(user){
-        return{
-            ...user,
-            avatar: getPublicURL(user.avatar),
-            cover: getPublicURL(user.cover)
-        }
-    }
+   if (user) {
+      return {
+         ...user,
+         avatar: getPublicURL(user.avatar),
+         cover: getPublicURL(user.cover),
+      };
+   }
 
-    return null;
-}
+   return null;
+};
 
-export const findUserBySlug = async (slug:string) => {
-    const user = await prisma.user.findFirst({
-        select: {
-            avatar: true,
-            cover: true,
-            slug: true,
-            bio: true,
-            link: true
-        },
-        where: { slug }
-    });
+export const findUserBySlug = async (slug: string) => {
+   const user = await prisma.user.findFirst({
+      select: {
+         avatar: true,
+         cover: true,
+         slug: true,
+         bio: true,
+         link: true,
+         name: true,
+      },
+      where: { slug },
+   });
 
-    if(user){
-        return{
-            ...user,
-            avatar: getPublicURL(user.avatar),
-            cover: getPublicURL(user.cover)
-        }
-    }
+   if (user) {
+      return {
+         ...user,
+         avatar: getPublicURL(user.avatar),
+         cover: getPublicURL(user.cover),
+      };
+   }
 
-    return null;
-}
+   return null;
+};
 
-export const createUser = async (data:Prisma.UserCreateInput) => {
-    const newUser = await prisma.user.create({ data });
+export const createUser = async (data: Prisma.UserCreateInput) => {
+   const newUser = await prisma.user.create({ data });
 
-    return{
-        ...newUser,
-        avatar: getPublicURL(newUser.avatar),
-        cover: getPublicURL(newUser.cover)
-    }
-    
-}
+   return {
+      ...newUser,
+      avatar: getPublicURL(newUser.avatar),
+      cover: getPublicURL(newUser.cover),
+   };
+};
 
 export const getUserFollowingCount = async (slug: string) => {
-    const count = await prisma.follow.count({
-        where: { user1Slug: slug }
-    });
-    return count;
-}
+   const count = await prisma.follow.count({
+      where: { user1Slug: slug },
+   });
+   return count;
+};
 
 export const getUserFollowersCount = async (slug: string) => {
-    const count = await prisma.follow.count({
-        where: { user2Slug: slug }
-    });
-    return count;
-}
+   const count = await prisma.follow.count({
+      where: { user2Slug: slug },
+   });
+   return count;
+};
 
 export const getUserPostsCount = async (slug: string) => {
-    const count = await prisma.post.count({
-        where: { userSlug: slug }
-    });
-    return count;
-}
+   const count = await prisma.post.count({
+      where: { userSlug: slug },
+   });
+   return count;
+};
 
-export const findPostsByUser = async (slug: string, currentPage: number, perPage: number) => {
-    const posts = await prisma.post.findMany({
-        include: {
-            user: {
-                select: {
-                    name: true,
-                    avatar: true,
-                    slug: true
-                }
+export const findPostsByUser = async (
+   slug: string,
+   currentPage: number,
+   perPage: number
+) => {
+   const posts = await prisma.post.findMany({
+      include: {
+         user: {
+            select: {
+               name: true,
+               avatar: true,
+               slug: true,
             },
-            likes: {
-                select: {
-                    userSlug: true
-                }
-            }
-        },
-        where: { userSlug: slug, commentOf: 0 },
-        orderBy: { createdAt: 'desc' },
-        skip: currentPage * perPage,
-        take: perPage
-    });
+         },
+         likes: {
+            select: {
+               userSlug: true,
+            },
+         },
+      },
+      where: { userSlug: slug, commentOf: 0 },
+      orderBy: { createdAt: "desc" },
+      skip: currentPage * perPage,
+      take: perPage,
+   });
 
-    return posts;
-}
+   return posts;
+};
 
 export const checkIfFollows = async (user1Slug: string, user2Slug: string) => {
-    const follows = await prisma.follow.findFirst({
-        where: { user1Slug, user2Slug }
-    });
+   const follows = await prisma.follow.findFirst({
+      where: { user1Slug, user2Slug },
+   });
 
-    return follows ? true : false;
-}
+   return follows ? true : false;
+};
 
 export const followUser = async (user1Slug: string, user2Slug: string) => {
-    await prisma.follow.create({
-        data: { user1Slug, user2Slug }
-    });
-}
+   await prisma.follow.create({
+      data: { user1Slug, user2Slug },
+   });
+};
 
 export const unfollowUser = async (user1Slug: string, user2Slug: string) => {
-    await prisma.follow.deleteMany({
-        where: { user1Slug, user2Slug }
-    });
-}
+   await prisma.follow.deleteMany({
+      where: { user1Slug, user2Slug },
+   });
+};
 
-export const updateUserInfo = async (slug: string, data: Prisma.UserUpdateInput) => {
-    await prisma.user.update({ 
-        where: { slug },
-        data
-    });
-}
+export const updateUserInfo = async (
+   slug: string,
+   data: Prisma.UserUpdateInput
+) => {
+   await prisma.user.update({
+      where: { slug },
+      data,
+   });
+};
 
 export const getUserFollowing = async (slug: string) => {
-    const following = [];
-    const reqFollow = await prisma.follow.findMany({
-        select: {
-            user2Slug: true
-        },
-        where: { user1Slug: slug }
-    });
+   const following = [];
+   const reqFollow = await prisma.follow.findMany({
+      select: {
+         user2Slug: true,
+      },
+      where: { user1Slug: slug },
+   });
 
-    for(let follow of reqFollow){
-        following.push(follow.user2Slug);
-    }
+   for (let follow of reqFollow) {
+      following.push(follow.user2Slug);
+   }
 
-    return following;
-}
+   return following;
+};
 
 export const getFollowSuggestions = async (slug: string) => {
-    const following = await getUserFollowing(slug);
+   const following = await getUserFollowing(slug);
 
-    const followingPlusMe = [...following, slug];
+   const followingPlusMe = [...following, slug];
 
-    type Suggestion = Pick<
-        Prisma.UserGetPayload<Prisma.UserDefaultArgs>,
-        "name" | "avatar" | "slug"
-    >;
+   type Suggestion = Pick<
+      Prisma.UserGetPayload<Prisma.UserDefaultArgs>,
+      "name" | "avatar" | "slug"
+   >;
 
-    const suggestions: Suggestion[] = await prisma.$queryRaw`
+   const suggestions: Suggestion[] = await prisma.$queryRaw`
         SELECT
             name, avatar, slug
         FROM "User"
         WHERE
-            slug NOT IN (${followingPlusMe.join(',')})
+            slug NOT IN (${followingPlusMe.join(",")})
         ORDER BY RANDOM()
         LIMIT 2;
     `;
 
-    for(let slugIndex in suggestions) {
-        suggestions[slugIndex].avatar = getPublicURL(suggestions[slugIndex].avatar);
-    }
-}
+   for (let slugIndex in suggestions) {
+      suggestions[slugIndex].avatar = getPublicURL(
+         suggestions[slugIndex].avatar
+      );
+   }
+};
 
 export const saveImage = async (slug: string, data: Prisma.UserUpdateInput) => {
-    await prisma.user.update({ 
-        where: { slug },
-        data
-    });
-}
+   await prisma.user.update({
+      where: { slug },
+      data,
+   });
+};
